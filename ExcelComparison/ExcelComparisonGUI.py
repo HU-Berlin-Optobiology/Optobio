768cf27bbd4af1c95113ee7fe0d7a3898c0ec131
HU-Berlin-Optobiology <141144847+HU-Berlin-Optobiology@users.noreply.github.com>
2025-03-18 11:07:24 +0100

    Add files via upload

--- ExcelComparison/ExcelComparisonGUI.py
+++ ExcelComparison/ExcelComparisonGUI.py
@@ -6,7 +6,7 @@ Created on Fri Feb 28 09:43:41 2025
 """
 
 from tkinter import ttk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from tkinter import *
 import pandas as pd
 import numpy as np
@@ -34,6 +34,9 @@ style.configure("C.TFrame", background="black")
 active0 = False
 active1 = False
 active2 = False
+start0 = False
+start1 = False
+start2 = False
 file_type = [("Excel", "*.xlsx")]
 
 ####################################################################################################################################
@@ -45,26 +48,32 @@ def destroyRoot(*event):
 def loadExcelUser(*event):
     global  LoadUser
     global active0
-    LoadUser = filedialog.askopenfilename(parent=root, title="Die Excel-Tabelle in der HU-Box auswählen", initialdir="Z:/", filetypes=file_type, initialfile="")
+    LoadUser = filedialog.askopenfilename(parent=root, title="Die Excel-Tabelle in der HU-Box auswählen", initialdir="Z:/HU_Box/orga lists (AB, constructs)", filetypes=file_type, initialfile="")
     path_load_user.set(LoadUser)
     active0 = True
+    start0 = True
+    if active0 and active1 and start0:
+        threading.Thread(target=LoadTables, daemon=True).start()
     if active0 and active1 and active2:
         CompareBtn.config(state=ACTIVE)
     
 def loadExcelBackup(*event):
     global  LoadBackup
     global active1
-    
     LoadBackup = filedialog.askopenfilename(parent=root, title="Die originale Excel-Tabelle auswählen", initialdir="C:/Users/", filetypes=file_type)
     path_load_backup.set(LoadBackup)
     active1 = True
+    start1 = True
+    if active0 and active1 and start1:
+        threading.Thread(target=LoadTables, daemon=True).start()
     if active0 and active1 and active2:
         CompareBtn.config(state=ACTIVE)
     
 def saveResult(*event):
     global Save
     global active2
-    Save = filedialog.asksaveasfilename(parent=root, title="Wo die resultierende Excel-tabelle gespeichert werden soll und unter welchem Namen", initialdir="C:/Users/", defaultextension=".xlsx",
+    InitDir = ("/").join(LoadBackup.split("\\")[:-1])
+    Save = filedialog.asksaveasfilename(parent=root, title="Wo die resultierende Excel-tabelle gespeichert werden soll und unter welchem Namen", initialdir=InitDir, defaultextension=".xlsx",
                                         filetypes=file_type)
     path_save.set(Save)
     active2 = True
@@ -72,43 +81,105 @@ def saveResult(*event):
         CompareBtn.config(state=ACTIVE)
 
 def ShowWork(*event):
+    global thread1
     p_bar.grid(row=4, column=0, padx=10, pady=10, sticky=W, columnspan=4)
     p_bar.start()
-    threading.Thread(target=compareExcel, daemon=True).start()
-
+    thread1 = threading.Thread(target=compareExcel, daemon=True)
+    thread1.start()
     
-def compareExcel():
-    global wb
+def LoadTables():
+    global xl1
+    global xl2
+    global chosen
     file1 = LoadBackup
     xl1 = pd.ExcelFile(file1)
     file2 = LoadUser
     xl2 = pd.ExcelFile(file2)
-    sheets = xl1.sheet_names
-    df1 = pd.read_excel(xl1, sheet_name=sheets[0])
-    df2 = pd.read_excel(xl2, sheet_name=sheets[0])
-    #init of excel sheet and book
-    wb = openpyxl.Workbook()
-    ws = wb.active
-    ws.title = "Fehlend"
-    keys = df1.keys()
-    r=1
-    c=1
-    for i in range(len(keys)):
-        value = keys[i]
-        ws.cell(row=r, column=c+1, value=value)
-    r+=1
-    #end of init
-    for i in range(len(df1)):
-        for j in range(len(df2)):
-            backup = df1.loc[i].tolist()
-            user = df2.loc[j].tolist()
-            if backup==user:
-                break
-            elif j==len(df2)-1:
-                for k in range(len(backup)):
-                    value=backup[k]
-                    ws.cell(row=r, column=c+k, value=value)
+    sheets1 = xl1.sheet_names
+    sheets2 = xl2.sheet_names
+    chosen = StringVar()
+    if sheets1!=sheets2:
+        messagebox.showerror("Nicht identische Tabellen", "Die Tabellen haben unterschiedliche Benennungen der Arbeitsblätter.")
+        active0 = False
+        active1 = False
+        start0 = False
+        start1 = False
+    else:
+        # Drop Down menu for sheets
+        sheets = ["Arbeitsblatt"]+sheets1+["Alle"]
+        SheetsDrop = ttk.OptionMenu(root, chosen, *sheets)
+        SheetsDrop.grid(row=3, column=1, columnspan=3, padx=10, pady=10, sticky=W)
+    
+def compareExcel():
+    global wb
+    compare = chosen.get()
+    if compare == "Alle":
+        sheets = xl1.sheet_names
+        for sheet in sheets:
+            df1 = pd.read_excel(xl1, sheet_name=sheet).replace(np.nan, "\xa0")
+            df2 = pd.read_excel(xl2, sheet_name=sheet).replace(np.nan, "\xa0")
+            if sheet == sheets[0]:
+                #init of excel sheet and book
+                wb = openpyxl.Workbook()
+                ws = wb.active
+                ws.title = sheet+"_Fehlend"
+                keys = df1.keys()
+                r=1
+                c=1
+                for i in range(len(keys)):
+                    value = keys[i]
+                    ws.cell(row=r, column=c+i, value=value)
                 r+=1
+                #end of init
+            elif sheet != sheets[0]:
+                #init of excel sheet and book
+                ws = wb.create_sheet(sheet+"Fehlend")
+                keys = df1.keys()
+                r=1
+                c=1
+                for i in range(len(keys)):
+                    value = keys[i]
+                    ws.cell(row=r, column=c+i, value=value)
+                r+=1
+                #end of init
+            for i in range(len(df1)):
+                for j in range(len(df2)):
+                    backup = df1.loc[i].tolist()
+                    user = df2.loc[j].tolist()
+                    if backup==user:
+                        break
+                    elif j==len(df2)-1:
+                        for k in range(len(backup)):
+                            value=backup[k]
+                            ws.cell(row=r, column=c+k, value=value)
+                        r+=1
+    elif compare != "Alle":  
+        df1 = pd.read_excel(xl1, sheet_name=compare).replace(np.nan, "\xa0")
+        df2 = pd.read_excel(xl2, sheet_name=compare).replace(np.nan, "\xa0")
+        #init of excel sheet and book
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        ws.title = "Fehlend"
+        keys = df1.keys()
+        r=1
+        c=1
+        for i in range(len(keys)):
+            value = keys[i]
+            ws.cell(row=r, column=c+i, value=value)
+        r+=1
+        #end of init
+        for i in range(len(df1)):
+            for j in range(len(df2)):
+                backup = df1.loc[i].tolist()
+                user = df2.loc[j].tolist()
+                if backup==user:
+                    break
+                elif j==len(df2)-1:
+                    for k in range(len(backup)):
+                        value=backup[k]
+                        ws.cell(row=r, column=c+k, value=value)
+                    r+=1
+
     p_bar.grid_forget()
     p_bar.stop()
     SaveBtn.config(state=ACTIVE)
@@ -171,6 +242,7 @@ CompareBtn.bind_all("<Return>", compareExcel)
 SaveBtn = ttk.Button(root, text="Speichern", command=save, style="C.TButton", state=DISABLED)
 SaveBtn.grid(row=3, column=4, padx=10, pady=10)
 SaveBtn.bind_all("<Return>", save)
+
 
 ####################################################################################################################################
 # Labels
