99796ea20e4d1caa95be0772dea29bae88873577
HU-Berlin-Optobiology <141144847+HU-Berlin-Optobiology@users.noreply.github.com>
2025-05-22 11:53:13 +0200

    Add files via upload
    Python-code for renaming file to fit name pattern for the ScanSlideStitching macro

---
+++ ScanSlideStitching/RenamingFilesForStitching.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 22 11:15:51 2025
+
+@author: Erich
+"""
+import os
+
+# Path to files to be renamed; needs the / seperator and not the windows \ one!
+# Path can also contain subfolders containing files to be renamed
+# important is, that file type and method for acquisition is the same
+path = ""
+
+# file type of files to be renamed; activate one that fits your type or make your own
+ftype = ".ome.tif"
+# ftype = ".stk"
+# ftype = ".tif"
+# ftype = ""
+
+# Method used to acquire your data; activate your method or make it
+method = "wf"
+# method = ""
+
+# Method to replace method used to acquire data
+Method = "Conf" # HH_SDC
+# Method = "sdc" # SDC
+
+#####################################################################################################################
+### End of user input
+#####################################################################################################################
+
+files = [os.path.join(root, file) for root, dirs, files in os.walk(path) for file in files]
+WL = str(405) # To replace DAPI with 405
+
+for f in files:
+    if f.endswith(ftype):
+        old = f
+        new = f.replace(method, Method)
+        if "DAPI" in new:
+            new = new.replace("DAPI", WL)
+        # print(f"Old: {old}\n New: {new}\n\n") # Testing before renaming stuff without knowing output
+        os.rename(old, new)
+        
