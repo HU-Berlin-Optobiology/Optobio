c217b62e76bfb259e58ebe8734aee957972a11cc
HU-Berlin-Optobiology <141144847+HU-Berlin-Optobiology@users.noreply.github.com>
2025-06-16 12:46:27 +0200

    Added option to view changes first
    added boolean "printing" to allow user to switch between printing resulting renaming and renaming files

--- ScanSlideStitching/RenamingFilesForStitching.py
+++ ScanSlideStitching/RenamingFilesForStitching.py
@@ -11,20 +11,26 @@ import os
 # important is, that file type and method for acquisition is the same
 path = ""
 
+# boolean; whether you want to view first how the renaming looks like
+# True: show how the renaming looks like without renaming files
+# False: rename files
+printing = False
+
 # file type of files to be renamed; activate one that fits your type or make your own
 # Keep only one of them active and silence the others with a # at the beginning of the line
+# Silencing/Activating line of code can be done using hotkey Ctrl+1
 ftype = ".ome.tif"
 # ftype = ".stk"
 # ftype = ".tif"
 # ftype = ""
 
 # Method used to acquire your data; activate your method or make it
-# Keep only one of them active and silence the others with a # at the beginning of the line
+# Keep only one of them active and silence the others with hotkey Ctrl+1
 method = "wf"
 # method = ""
 
 # Method to replace method used to acquire data
-# Keep only one of them active and silence the others with a # at the beginning of the line
+# Keep only one of them active and silence the others with hotkey Ctrl+1
 Method = "Conf" # HH_SDC
 # Method = "sdc" # SDC
 # Method = "" # Whatever you want
@@ -42,5 +48,7 @@ for f in files:
         new = f.replace(method, Method)
         if "DAPI" in new:
             new = new.replace("DAPI", WL)
-        # print(f"Old: {old}\n New: {new}\n\n") # Testing before renaming stuff without knowing output
-        os.rename(old, new)
+        if printing:
+            print(f"Old: {old}\n New: {new}\n\n") # Testing before renaming stuff without knowing output
+        elif not printing:
+            os.rename(old, new)
